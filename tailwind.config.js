55273818a11320269119b0ad1c2e9bd6a91c3aff
mohammedhayat <mohammedhayat3516@gmail.com>
2025-02-01 21:49:44 +0300

    first commite

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ['./*.html'],
+    theme: {
+      screens: {
+        'sm': '30rem',    // 480px
+        'md': '48rem',    // 768px
+        'lg': '61rem',    // 976px
+        'xl': '90rem',    // 1440px
+        '2xl': '93.75rem' // 1500px
+      },
+    },
+    plugins: [],
+  }
+  
